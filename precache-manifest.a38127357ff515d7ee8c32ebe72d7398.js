a912c2395d8123b02dbf602b008fa177c41caa89
Eloumbat Assoua Albert <assouaalbert@gmail.com>
2020-05-31 05:41:09 +0300

    Updates

---
+++ precache-manifest.a38127357ff515d7ee8c32ebe72d7398.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48c5484ec7c7b3df9d37babfe7b8ed08",
+    "url": "/Monster-Rolodex-App/index.html"
+  },
+  {
+    "revision": "8646cf5cf6e1167badc3",
+    "url": "/Monster-Rolodex-App/static/css/main.d43e8ffb.chunk.css"
+  },
+  {
+    "revision": "d687c1f9d751eecf4fb8",
+    "url": "/Monster-Rolodex-App/static/js/2.1dd7b907.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Rolodex-App/static/js/2.1dd7b907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8646cf5cf6e1167badc3",
+    "url": "/Monster-Rolodex-App/static/js/main.4e1840e0.chunk.js"
+  },
+  {
+    "revision": "e5c4b716f508b980f8ea",
+    "url": "/Monster-Rolodex-App/static/js/runtime-main.9484ec2d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Monster-Rolodex-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
